23d7f770e51a149bcad244ee1e1840f4f1acce93
Anurag Shekhawat <anuragshekhawat1995@gmail.com>
2020-06-24 22:39:13 +0530

    Cards infected data

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Card, CardContent,Typography,Grid} from '@material-ui/core'
-
+import CountUp from 'react-countup';
 import styles from './Cards.module.css'
 
 const Cards = ({data:{confirmed,recovered,deaths,lastUpdate}})=>{
@@ -14,8 +14,10 @@ const Cards = ({data:{confirmed,recovered,deaths,lastUpdate}})=>{
                 <Grid item component = {Card}>
                     <CardContent>
                         <Typography color = "textSecondary" gutterBottom>Infected</Typography>
-                        <Typography variant = "h5">{confirmed.value}</Typography>
-                        <Typography variant = "textSecondary">REAL DATE</Typography>
+                        <Typography variant = "h5">
+                            <CountUp start = {0} end = {confirmed.value} duration = {2.5} separator = ","/>
+                        </Typography>
+                        <Typography variant = "textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                         <Typography variant = "body2">Number of active cases of COVID-19</Typography>
                     </CardContent>
                 </Grid>
