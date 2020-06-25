41dd804f843add96c656fd0f6f0e26cfe98d9e9a
Anurag Shekhawat <anuragshekhawat1995@gmail.com>
2020-06-25 14:13:06 +0530

    Charts initials

--- src/Components/Chart/Chart.jsx
+++ src/Components/Chart/Chart.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
+import {fetchDailyData} from '../../api';
+
+import {Line,Bar} from 'react-chartjs-2';
+
+import styles from './Chart.module.css'
 
 const Chart = (props)=>{
+    const [dailyData, setDailyData] = useState({});
     console.log(props)
+    useEffect(()=>{
+        const fetchAPI = async () =>{
+            setDailyData(await fetchDailyData());
+        }
+        fetchAPI();
+    }
+    );
+
+
+    const lineChart = (
+        <Line>
+            data = {{
+                labels: '',
+                datasets:[{},{}]
+            }}
+        </Line>
+    );
+
     return (
         <h1>Charts</h1>
     )
